7a8367fd9de4b6619e20686ed62fda670701d695
Mustafa Fatih GUCLUER <fatihgucluer@hotmail.com>
2023-12-05 09:25:54 +0300

    Initial Commit

---
+++ programmatic1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  programmatic1
+//
+//  Created by Mustafa Fatih on 5/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
